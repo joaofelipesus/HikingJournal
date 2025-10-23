7e5d2d6c501eef4093aca6d2b6c06db1279ad892
joaofelipe <joaofelipe1294@gmail.com>
2025-10-23 07:42:09 -0300

    add: google maps

--- app/src/main/java/com/lopes/hikingjournal/fragments/MapFragment.kt
+++ app/src/main/java/com/lopes/hikingjournal/fragments/MapFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.tooling.preview.Preview
 import dev.hotwire.navigation.fragments.HotwireFragment
 import com.lopes.hikingjournal.R
 import dev.hotwire.navigation.destinations.HotwireDestinationDeepLink
+import com.google.maps.android.compose.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberMarkerState
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapType
 
 @HotwireDestinationDeepLink(uri = "hotwire://fragment/map")
 class MapFragment : HotwireFragment() {
@@ -29,9 +35,21 @@ class MapFragment : HotwireFragment() {
         return view
     }
 
-    @Preview
     @Composable
-    fun MapView () {
-        Text(text = "Hello world!")
+    fun MapView() {
+        val coordinate = LatLng(45.511881, -122.595706)
+        val markerState = rememberMarkerState(position = coordinate)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(coordinate, 15f)
+        }
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+            properties = MapProperties(mapType = MapType.HYBRID)
+        ) {
+            Marker(
+                state = markerState,
+                title = "Mount Tabor"
+            )
+        }
     }
 }
