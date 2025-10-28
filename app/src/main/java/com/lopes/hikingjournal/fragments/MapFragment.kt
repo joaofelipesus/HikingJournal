4fbec49de87037ee2eb3f46b5f209df201cc57c3
joaofelipe <joaofelipe1294@gmail.com>
2025-10-28 08:17:42 -0300

    show coordinates

--- app/src/main/java/com/lopes/hikingjournal/fragments/MapFragment.kt
+++ app/src/main/java/com/lopes/hikingjournal/fragments/MapFragment.kt
@@ -11,36 +11,45 @@ import com.lopes.hikingjournal.R
 import dev.hotwire.navigation.destinations.HotwireDestinationDeepLink
 import com.google.maps.android.compose.GoogleMap
 import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
+import com.lopes.hikingjournal.viewmodels.HikeViewModel
+import androidx.compose.runtime.LaunchedEffect
 
 @HotwireDestinationDeepLink(uri = "hotwire://fragment/map")
 class MapFragment : HotwireFragment() {
+    private lateinit var viewModel : HikeViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        viewModel = HikeViewModel("${navigator.location}.json")
         val view = inflater.inflate(R.layout.fragment_map, container, false)
         view.findViewById<ComposeView>(R.id.compose_view).apply {
             setContent {
-                MapView()
+                MapView(viewModel)
             }
         }
 
         return view
     }
+}
 
-    @Composable
-    fun MapView() {
-        val coordinate = LatLng(45.511881, -122.595706)
-        val markerState = rememberMarkerState(position = coordinate)
+@Composable
+fun MapView(viewModel : HikeViewModel) {
+    LaunchedEffect(viewModel) {
+        viewModel.fetchCoordinates()
+    }
+    val hike = viewModel.hike.value
+    if (hike != null) {
+        val markerState = rememberMarkerState(position = hike.coordinate)
         val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(coordinate, 15f)
+            position = CameraPosition.fromLatLngZoom(hike.coordinate, 15f)
         }
         GoogleMap(
             cameraPositionState = cameraPositionState,
@@ -48,7 +57,7 @@ class MapFragment : HotwireFragment() {
         ) {
             Marker(
                 state = markerState,
-                title = "Mount Tabor"
+                title = hike.name
             )
         }
     }
